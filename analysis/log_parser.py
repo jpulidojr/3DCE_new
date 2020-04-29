d656a95c6c4545a86ee0dc7ce5a001733f82e1a0
Jesus Pulido <jpulidojr@gmail.com>
2020-04-29 16:50:56 -0600

    parse the rest of the quantities

--- analysis/log_parser.py
+++ analysis/log_parser.py
@@ -12,10 +12,14 @@ filename = "log.traintest_04-16_09-45-43.3DCE 1 image 3 slice" #Latest run with 
 epoch=[]
 epoch_time=[]
 epoch_time_accumulated=[]
-epoch_validate=[]
+epoch_sensitivity=[] # Same as recall
+epoch_precision=[]
+epoch_recall=[] # same as racall
+epoch_fmeasure=[]
 epochn=0
 
 fio = open(log_path+filename, "r")
+prev_line=""
 for line in fio:
     if 'Time cost' in line:
         #print(line)
@@ -31,30 +35,47 @@ for line in fio:
         else:
             epoch_time_accumulated.append(epoch_time_accumulated[-1]+float(s[1]))
         
-    #Note: The validation criteria for 3DCE uses Sensitivity @ 4, in the log files.    
+    #Note: The validation criteria for 3DCE uses Sensitivity @ 4, in the log files.
+    #Note2: Sensitivity is the same as Recall    
     if 'Sensitivity' in line:
         #print(line)
         s=line.split(':')
         s[-1] = s[-1].strip() #remove the \n
         r = s[-1].split()
         #print(r)
-        epoch_validate.append(float(r[3]))
+        epoch_sensitivity.append(float(r[3]))
 
+    # The log file prints the results below the string, so query prev line
+    if 'precision' in prev_line:
+        s=line.split()
+        epoch_precision.append(float(s[3]))
+    if 'recall' in prev_line:
+        s=line.split()
+        epoch_recall.append(float(s[3]))
+    if 'f-measure' in prev_line:
+        s=line.split()
+        epoch_fmeasure.append(float(s[3]))
+
+    prev_line=line
 
 fio.close()
 
-print(epoch)
-print(epoch_time)
-print(epoch_time_accumulated)
-print(epoch_validate)
+#print(epoch)
+#print(epoch_time)
+#print(epoch_time_accumulated)
+#print(epoch_sensitivity)
+#print(epoch_precision)
+#print(epoch_recall)
+#print(epoch_fmeasure)
 
-plt.plot(epoch, epoch_validate)
-plt.ylabel('sensitivity')
+#plt.plot(epoch, epoch_sensitivity, epoch, epoch_precision, epoch, epoch_fmeasure)
+plt.plot(epoch, epoch_sensitivity )
+plt.ylabel('recall (sensitivity)')
 plt.xlabel('# of epochs')
 plt.show()
 
 
-plt.plot(epoch_time_accumulated, epoch_validate)
-plt.ylabel('sensitivity')
+plt.plot(epoch_time_accumulated, epoch_sensitivity)
+plt.ylabel('recall (sensitivity)')
 plt.xlabel('epoch_time (s)')
 plt.show()
